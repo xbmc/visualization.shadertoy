9c8c341136f866773c5d4d52a663aed58b84d2a1
popcornmix <popcornmix@gmail.com>
2015-05-20 23:35:30 +0100

    speed up

--- visualization.shadertoy/resources/audiovisual.frag.glsl
+++ visualization.shadertoy/resources/audiovisual.frag.glsl
@@ -7,16 +7,21 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = fragCoord.xy / iResolution.xy;  
     float wave = texture2D( iChannel0, vec2(uv.x,0.75) ).x;
 
-    for(int i = 1; i<20; i++)
+    float time = 0.0;
+    float alpha = 2.*3.14/20.* (iGlobalTime*.9);
+    float cs = cos(iGlobalTime*.5);
+    float sn = sin(iGlobalTime*.3);
+    float sn2 = sin(iGlobalTime);
+    float sm=smoothstep( 0.0, 0.15, abs(wave - uv.y));
+    for(int i = 1; i<10; i++)
     {
-        float time = 2.*3.14*float(i)/20.* (iGlobalTime*.9);
-        float x = sin(time)*1.8*smoothstep( 0.0, 0.15, abs(wave - uv.y));
-        float y = sin(.5*time) *smoothstep( 0.0, 0.15, abs(wave - uv.y));
-        y*=.5;
-        vec2 o = .4*vec2(x*cos(iGlobalTime*.5),y*sin(iGlobalTime*.3));
+        time += alpha;
+        float x = sin(time)*1.8*sm;
+        float y = sin(.5*time)*0.5*sm;
+        vec2 o = .4*vec2(x*cs,y*sn);
         float red = fract(time);
         float green = 1.-red;
-        c+=0.016/(length(p-o))*vec3(red,green,sin(iGlobalTime));
+        c+=0.016/(length(p-o))*vec3(red,green,sn2);
     }
     fragColor = vec4(c,1.0);
 }
