5818a423df2a15ec618735a0955de772750ebc93
Alwin Esch <alwin.esch@web.de>
2017-04-26 11:40:43 +0200

    Remove not used part

--- src/main.cpp
+++ src/main.cpp
@@ -1136,16 +1136,6 @@ extern "C" void ADDON_Destroy()
   initialized = false;
 }
 
-//-- HasSettings --------------------------------------------------------------
-// Returns true if this add-on use settings
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-extern "C" bool ADDON_HasSettings()
-{
-  cout << "ADDON_HasSettings" << std::endl;
-  return true;
-}
-
 //-- GetStatus ---------------------------------------------------------------
 // Returns the current Status of this visualisation
 // !!! Add-on master function !!!
@@ -1154,26 +1144,6 @@ extern "C" ADDON_STATUS ADDON_GetStatus()
 {
   cout << "ADDON_GetStatus" << std::endl;
   return ADDON_STATUS_OK;
-}
-
-//-- GetSettings --------------------------------------------------------------
-// Return the settings for XBMC to display
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-extern "C" unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet)
-{
-  cout << "ADDON_GetSettings" << std::endl;
-  return 0;
-}
-
-//-- FreeSettings --------------------------------------------------------------
-// Free the settings struct passed from XBMC
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-
-extern "C" void ADDON_FreeSettings()
-{
-  cout << "ADDON_FreeSettings" << std::endl;
 }
 
 //-- SetSetting ---------------------------------------------------------------
@@ -1216,13 +1186,4 @@ extern "C" ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void* val
   }
 
   return ADDON_STATUS_UNKNOWN;
-}
-
-//-- Announce -----------------------------------------------------------------
-// Receive announcements from XBMC
-// !!! Add-on master function !!!
-//-----------------------------------------------------------------------------
-extern "C" void ADDON_Announce(const char *flag, const char *sender, const char *message, const void *data)
-{
-  cout << "ADDON_Announce " << flag << " " << sender << " " << message << std::endl;
 }
